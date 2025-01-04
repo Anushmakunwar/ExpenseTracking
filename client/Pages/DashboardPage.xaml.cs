6ad4c92238fce8c4e20c02cec11139a838e72f3e
Anushma kunwar <np01cp4s230051@islingtoncollege.edu.np>
2025-01-05 00:31:31 +0545

    pages ui/logic

---
+++ client/Pages/DashboardPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Xamarin.Forms;  // Make sure to include this namespace for Xamarin.Forms
+
+namespace ExpenseTrakingSystem
+{
+    internal class DashboardPage : ContentPage  // Inherit from ContentPage
+    {
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            if (!Preferences.ContainsKey("AuthToken"))
+            {
+                Shell.Current.GoToAsync("//LoginPage");
+            }
+        }
+    }
+}
